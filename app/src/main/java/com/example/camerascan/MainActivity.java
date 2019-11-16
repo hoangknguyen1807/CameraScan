7979897e9f700501d1b680922f3af81f1a00d665
Nguyễn Khánh Hoàng <hoangknguyen.1807@gmail.com>
2019-11-16 16:19:11 +0700

    Unfinished open Image from Storage

--- app/src/main/java/com/example/camerascan/MainActivity.java
+++ app/src/main/java/com/example/camerascan/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.camerascan;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.graphics.PointF;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import java.io.File;
 
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.MediaType;
 import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
@@ -21,27 +38,19 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 
 
-public class MainActivity extends Activity implements View.OnTouchListener
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final int REQUEST_PERMISSION_STORAGE = 1;
 
-{
-    private static final String TAG = "Touch";
-    @SuppressWarnings("unused")
-    private static final float MIN_ZOOM = 1f,MAX_ZOOM = 1f;
+    public static final int OPEN_IMAGE_CODE = 7;
 
-    // These matrices will be used to scale points of the image
-    Matrix matrix = new Matrix();
-    Matrix savedMatrix = new Matrix();
+    private ImageView imgView;
+    private Bitmap mainBitmap;
+    private Dialog loadingDialog;
+    private int imgWidth, imgHeight;
 
-    // The 3 states (events) which the user is trying to perform
-    static final int NONE = 0;
-    static final int DRAG = 1;
-    static final int ZOOM = 2;
-    int mode = NONE;
+    private String path;
 
-    // these PointF objects are used to record the point(s) the user is touching
-    PointF start = new PointF();
-    PointF mid = new PointF();
-    float oldDist = 1f;
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     /** Called when the activity is first created. */
     @Override
@@ -50,169 +59,132 @@ public class MainActivity extends Activity implements View.OnTouchListener
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView view = (ImageView) findViewById(R.id.imageView);
-        view.setOnTouchListener(this);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        imgWidth = metrics.widthPixels;
+        imgHeight = metrics.heightPixels;
+
+        imgView = findViewById(R.id.imageView);
+
         Button btnUpload = findViewById(R.id.btnUpload);
         btnUpload.setOnClickListener(new View.OnClickListener() {
-
-
             @Override
             public void onClick(View view) {
-                uploadToServer();
-
             }
         });
+
+        Button openImage = findViewById(R.id.btnOpenImage);
+        openImage.setOnClickListener(this);
+
+        loadingDialog = BaseActivity.getLoadingDialog(this, R.string.loading,
+                false);
     }
 
-    public void uploadToServer(){
-        String filePath = "drawable://" + R.drawable.kimyen;
-        Retrofit retrofit = NetworkClient.getRetrofitClient(this);
-        UploadAPIs uploadAPIs = retrofit.create(UploadAPIs.class);
-        //Create a file object using file path
-        File file = new File(filePath);
-        // Create a request body with file and image media type
-        RequestBody fileReqBody = RequestBody.create(MediaType.parse("image/*"), file);
-        // Create MultipartBody.Part using file request-body,file name and part name
-        MultipartBody.Part part = MultipartBody.Part.createFormData("upload", file.getName(), fileReqBody);
-        //Create request body with text description and text media type
-        RequestBody description = RequestBody.create(MediaType.parse("text/plain"), "image-type");
-        //
-        Call call = UploadAPIs.uploadImage(part, description);
-        call.enqueue(new Callback() {
-            @Override
-            public void onResponse(Call call, Response response) {
-            }
-            @Override
-            public void onFailure(Call call, Throwable t) {
-            }
-        });
-    }
     @Override
-    public boolean onTouch(View v, MotionEvent event)
+    public void onClick(View view)
     {
-        ImageView view = (ImageView) v;
-        view.setScaleType(ImageView.ScaleType.MATRIX);
-        float scale;
-
-        dumpEvent(event);
-        // Handle touch events here...
-
-        switch (event.getAction() & MotionEvent.ACTION_MASK)
-        {
-            case MotionEvent.ACTION_DOWN:   // first finger down only
-                savedMatrix.set(matrix);
-                start.set(event.getX(), event.getY());
-                Log.d(TAG, "mode=DRAG"); // write to LogCat
-                mode = DRAG;
-                break;
-
-            case MotionEvent.ACTION_UP: // first finger lifted
-
-            case MotionEvent.ACTION_POINTER_UP: // second finger lifted
-
-                mode = NONE;
-                Log.d(TAG, "mode=NONE");
-                break;
-
-            case MotionEvent.ACTION_POINTER_DOWN: // first and second finger down
-
-                oldDist = spacing(event);
-                Log.d(TAG, "oldDist=" + oldDist);
-                if (oldDist > 5f) {
-                    savedMatrix.set(matrix);
-                    midPoint(mid, event);
-                    mode = ZOOM;
-                    Log.d(TAG, "mode=ZOOM");
-                }
-                break;
-
-            case MotionEvent.ACTION_MOVE:
-
-                if (mode == DRAG)
-                {
-                    matrix.set(savedMatrix);
-                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y); // create the transformation in the matrix  of points
-                }
-                else if (mode == ZOOM)
-                {
-                    // pinch zooming
-                    float newDist = spacing(event);
-                    Log.d(TAG, "newDist=" + newDist);
-                    if (newDist > 5f)
-                    {
-                        matrix.set(savedMatrix);
-                        scale = newDist / oldDist; // setting the scaling of the
-                        // matrix...if scale > 1 means
-                        // zoom in...if scale < 1 means
-                        // zoom out
-                        matrix.postScale(scale, scale, mid.x, mid.y);
-                    }
-                }
+        switch (view.getId()) {
+            case R.id.btnOpenImage:
+                this.openImageFromStorage();
                 break;
         }
-
-        view.setImageMatrix(matrix); // display the transformation on screen
-
-        return true; // indicate event was handled
     }
 
-    /*
-     * --------------------------------------------------------------------------
-     * Method: spacing Parameters: MotionEvent Returns: float Description:
-     * checks the spacing between the two fingers on touch
-     * ----------------------------------------------------
-     */
-
-    private float spacing(MotionEvent event)
-    {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        return (float) Math.sqrt(x * x + y * y);
-    }
-
-    /*
-     * --------------------------------------------------------------------------
-     * Method: midPoint Parameters: PointF object, MotionEvent Returns: void
-     * Description: calculates the midpoint between the two fingers
-     * ------------------------------------------------------------
-     */
-
-    private void midPoint(PointF point, MotionEvent event)
-    {
-        float x = event.getX(0) + event.getX(1);
-        float y = event.getY(0) + event.getY(1);
-        point.set(x / 2, y / 2);
-    }
-
-    /** Show an event in the LogCat view, for debugging */
-    private void dumpEvent(MotionEvent event)
-    {
-        String names[] = { "DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE","POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?" };
-        StringBuilder sb = new StringBuilder();
-        int action = event.getAction();
-        int actionCode = action & MotionEvent.ACTION_MASK;
-        sb.append("event ACTION_").append(names[actionCode]);
-
-        if (actionCode == MotionEvent.ACTION_POINTER_DOWN || actionCode == MotionEvent.ACTION_POINTER_UP)
-        {
-            sb.append("(pid ").append(action >> MotionEvent.ACTION_POINTER_ID_SHIFT);
-            sb.append(")");
+    private void openImageFromStorage() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            openImageWithPermissionsCheck();
+        } else {
+            openImage();
         }
-
-        sb.append("[");
-        for (int i = 0; i < event.getPointerCount(); i++)
-        {
-            sb.append("#").append(i);
-            sb.append("(pid ").append(event.getPointerId(i));
-            sb.append(")=").append((int) event.getX(i));
-            sb.append(",").append((int) event.getY(i));
-            if (i + 1 < event.getPointerCount())
-                sb.append(";");
-        }
-
-        sb.append("]");
-        Log.d("Touch Events ---------", sb.toString());
     }
 
+    private void openImageWithPermissionsCheck() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION_STORAGE);
+            return;
+        }
+        openImage();
+    }
+
+    private void openImage() {
+        MainActivity.this.startActivityForResult(new Intent(
+                        MainActivity.this,
+                        OpenImageActivity.class),
+                OPEN_IMAGE_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION_STORAGE
+                && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            openImage();
+        } /*else if (requestCode == REQUEST_PERMISSON_CAMERA
+                && grantResults.length > 0
+                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            launchCamera();
+        }*/
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case OPEN_IMAGE_CODE:
+                    handleOpenImageFromStorage(data);
+                    break;
+                /*case TAKE_PHOTO_CODE:
+                    handleTakePhoto();
+                    break;
+                case ACTION_REQUEST_EDITIMAGE:
+                    handleEditorImage(data);
+                    break;*/
+            }
+        }
+    }
+
+    private void handleOpenImageFromStorage(Intent data) {
+        path = data.getStringExtra("imgPath");
+        loadImage(path);
+    }
+
+    private void loadImage(String imagePath) {
+        compositeDisposable.clear();
+        Disposable applyRotationDisposable = loadBitmapFromFile(imagePath)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(subscriber -> loadingDialog.show())
+                .doFinally(() -> loadingDialog.dismiss())
+                .subscribe(
+                        this::setMainBitmap,
+                        e -> Toast.makeText(
+                                this, R.string.load_error, Toast.LENGTH_SHORT).show()
+                );
+
+        compositeDisposable.add(applyRotationDisposable);
+    }
+
+    private void setMainBitmap(Bitmap sourceBitmap) {
+        if (mainBitmap != null) {
+            mainBitmap.recycle();
+            mainBitmap = null;
+            System.gc();
+        }
+        mainBitmap = sourceBitmap;
+        imgView.setImageBitmap(mainBitmap);
+    }
+
+    private Single<Bitmap> loadBitmapFromFile(String filePath) {
+        return Single.fromCallable(() ->
+                BitmapUtils.getSampledBitmap(
+                        filePath,
+                        imgWidth / 4,
+                        imgHeight / 4
+                )
+        );
+    }
 
 }

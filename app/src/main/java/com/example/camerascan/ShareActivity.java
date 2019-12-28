bd7cad8b2379c7747e2e71172bbb353bb8b178d7
TrungTho <41388666+TrungTho@users.noreply.github.com>
2019-12-28 17:19:18 +0700

    fix share func for android 6 upper

--- app/src/main/java/com/example/camerascan/ShareActivity.java
+++ app/src/main/java/com/example/camerascan/ShareActivity.java
@@ -7,6 +7,7 @@ import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.ParcelFileDescriptor;
 import android.util.Log;
 import android.view.View;
@@ -18,10 +19,14 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URLConnection;
 
 public class ShareActivity extends Activity {
@@ -70,11 +75,22 @@ public class ShareActivity extends Activity {
                 }
                 else {
                     File fileName = new File(_fileUri.getPath());
-                    Intent intentToShareSheet = new Intent(Intent.ACTION_SEND);
 
-                    intentToShareSheet.setType(URLConnection.guessContentTypeFromName(fileName.getName()));
-                    intentToShareSheet.putExtra(Intent.EXTRA_STREAM,Uri.parse("file://"+fileName.getAbsolutePath()));
-                    startActivity(Intent.createChooser(intentToShareSheet, "Share File"));
+                    Uri forLolipop = FileProvider.getUriForFile(ShareActivity.this,"com.example.android.fileprovider",fileName);
+
+                    // Get the Uri from the external file and add it to the intent
+                    //Uri forLolipop = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), _fileUri.getPath()));
+
+                    Intent intentToShareSheet = new Intent(Intent.ACTION_SEND,forLolipop);
+                    intentToShareSheet.setType("image/*");
+                    intentToShareSheet.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                   // intentToShareSheet.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+
+                    //intentToShareSheet.setData(forLolipop);
+                    intentToShareSheet.putExtra(Intent.EXTRA_STREAM,forLolipop);
+                    startActivity(Intent.createChooser(intentToShareSheet, "OCR Share File"));
+
+
                 }
             }
         });
@@ -83,7 +99,9 @@ public class ShareActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent requestFileIntent = new Intent(Intent.ACTION_GET_CONTENT);
-                requestFileIntent.setType("file/*");
+                requestFileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                requestFileIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                requestFileIntent.setType("*/*");
                 startActivityForResult(requestFileIntent, REQUEST_PICK_FILE);
             }
         });
@@ -93,7 +111,7 @@ public class ShareActivity extends Activity {
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         if (_fileUri!=null)
-            outState.putString("tmpUri",_fileUri.toString());
+            outState.putString("tmpUri",_fileUri.getPath());
     }
 
     @Override
@@ -104,15 +122,7 @@ public class ShareActivity extends Activity {
                 case REQUEST_PICK_FILE:
                     Uri fileUriToShare = data.getData();
                     _fileUri=fileUriToShare;
-                    textViewToShowPath.setText(_fileUri.toString());
-                    try {
-                        ParcelFileDescriptor inputPFD = getContentResolver().openFileDescriptor(fileUriToShare, "r");
-                        FileDescriptor fd = inputPFD.getFileDescriptor();
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                        Log.e("MainActivity", "File not found.");
-                        return;
-                    }
+                    textViewToShowPath.setText(_fileUri.getPath());
                     break;
             }
         } else if (resultCode == RESULT_CANCELED) {}
